b157cccb9b5a52f50e49eca350ab332ecf12dcaa
Leonardo Westley <leonardowestley@Leonardos-MacBook-Air.local>
2022-04-22 11:54:33 -0400

    project 1

---
+++ index.js
@@ -0,0 +1,14 @@
+const usdollarAmount = 35.63
+const otherCurrency = 51.30
+
+function calculateCurrency(currency, conversionRate) {
+    const conversion = currency * conversionRate
+    return conversion
+}
+calculateCurrency(usdollarAmount, otherCurrency);
+
+const newAmount = calculateCurrency(usdollarAmount, otherCurrency)
+
+console.log(newAmount.toFixed(2))
+
+
